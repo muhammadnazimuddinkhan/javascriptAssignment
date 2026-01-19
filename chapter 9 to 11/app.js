c3998b1b1795b6dd3afdd634be4c2f0e276f0a67
muhammadnazimuddinkhan <165476534+muhammadnazimuddinkhan@users.noreply.github.com>
2026-01-19 10:06:54 +0500

    fist commit

---
+++ chapter 9 to 11/app.js
@@ -0,0 +1,103 @@
+// Taking input from user
+// let num1 = parseFloat(prompt("Enter first number:"));
+// let num2 = parseFloat(prompt("Enter second number:"));
+// let op = prompt("Enter operation (+, -, *, /, %):");
+
+// Using if statements
+// if (op === "+") {
+//     alert("Result: " + (num1 + num2));
+// }
+// else if (op === "-") {
+//     alert("Result: " + (num1 - num2));
+// }
+// else if (op === "*") {
+//     alert("Result: " + (num1 * num2));
+// }
+// else if (op === "/") {
+//     if (num2 !== 0) {
+//         alert("Result: " + (num1 / num2));
+//     } else {
+//         alert("Error: Division by zero");
+//     }
+// }
+// else if (op === "%") {
+//     if (num2 !== 0) {
+//         alert("Result: " + (num1 % num2));
+//     } else {
+//         alert("Error: Modulus by zero");
+//     }
+// }
+// else {
+//     alert("Invalid operation");
+// }
+
+
+// Take temperature input from user
+// let T = parseFloat(prompt("Enter temperature:"));
+
+// Check temperature conditions
+// if (T > 40) {
+//     alert("It is too hot outside.");
+// }
+// else if (T > 30) {
+//     alert("The Weather today is Normal.");
+// }
+// else if (T > 20) {
+//     alert("Today’s Weather is cool.");
+// }
+// else if (T > 10) {
+//     alert("OMG! Today’s weather is so Cool.");
+// }
+// else {
+//     alert("It is very cold today.");
+// }
+// Take number input from user
+// let num = parseInt(prompt("Enter a number:"));
+
+// Check even or odd
+// if (num % 2 === 0) {
+//     alert("The number is Even");
+// }
+// else {
+//     alert("The number is Odd");
+// }
+
+// Take number input from user
+// let num = parseInt(prompt("Enter a number:"));
+
+// Check divisibility by 3
+// if (num % 3 === 0) {
+//     alert("The number is divisible by 3");
+// }
+// else {
+//     alert("The number is not divisible by 3");
+// }
+
+// Store secret number
+// let secretNumber = 7; // you can change this number (1-10)
+
+// Ask user to guess
+// let guess = parseInt(prompt("Guess the secret number (1 to 10):"));
+
+// Check the guess
+// if (guess === secretNumber) {
+//     alert("Bingo! Correct answer");
+// }
+// else if (guess + 1 === secretNumber || guess - 1 === secretNumber) {
+//     alert("Close enough to the correct answer");
+// }
+// else {
+//     alert("Try again!");
+// }
+// Take remaining fuel input from user
+// let fuel = parseFloat(prompt("Enter remaining fuel in your car (in litres):"));
+
+// Check fuel level
+// if (fuel < 0.25) {
+//     alert("Please refill the fuel in your car");
+// } else {
+//     alert("Fuel level is sufficient");
+// }
+
+
+
