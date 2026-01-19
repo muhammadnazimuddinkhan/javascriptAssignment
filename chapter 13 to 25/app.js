a39bc5b70e97988167507acae41a8ce5c1e85a99
muhammadnazimuddinkhan <165476534+muhammadnazimuddinkhan@users.noreply.github.com>
2026-01-19 14:41:18 +0500

    first commit

---
+++ chapter 13 to 25/app.js
@@ -0,0 +1,99 @@
+ // Step 1: Create an empty array
+        // var devices = [];
+
+        // // Step 2: Add values to the array one by one
+        // devices.push("keyboard");
+        // devices.push("mouse");
+        // devices.push("printer");
+        // devices.push("monitor");
+
+        // // Step 3: Access the values in reverse order using a loop
+        // console.log("Devices in reverse order:<br>");
+        // while (devices.length > 0) {
+        //     var out = devices.pop(); // Removes the last element and returns it
+        //     console.log(out + "<br>");
+        // }
+
+
+        // Given array
+        // var arr = ["This ", " is ", " my ", " cat"];
+
+        // // Create a single string using join()
+        // var result = arr.join("");
+
+        // // Display the result
+        // console.log(result);
+        // var cities = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"];
+
+        // // Copy 3 elements from cities to selectedCities
+        // var selectedCities = cities.slice(0, 3);
+
+        // // Display results
+        // console.log("Cities: " + cities + "<br>");
+        // console.log("Selected Cities: " + selectedCities);
+
+        // Store student scores in an array
+        // var scores = [320, 230, 480, 120];
+
+        // // Sort the array in ascending order
+        // scores.sort(function(a, b) {
+        //     return a - b;
+        // });
+
+        // // Display the sorted scores
+        // console.log("Sorted Scores: " + scores);
+
+         // Store student names
+        // var students = ["Ali", "Ahmed", "Sara"];
+
+        // // Store their scores
+        // var scores = [420, 380, 450];
+
+        // var totalMarks = 500;
+
+        // // Display result
+        // for (var i = 0; i < students.length; i++) {
+        //     var percentage = (scores[i] / totalMarks) * 100;
+        //     console.log(
+        //         "Score of " + students[i] + " is " + scores[i] +
+        //         ". Percentage: " + percentage + "%<br>"
+        //     );
+        // }
+         // Take user input
+        // var firstName = prompt("Enter your first name:");
+        // var lastName = prompt("Enter your last name:");
+
+        // // Merge names
+        // var fullName = firstName + " " + lastName;
+
+        // // Greet the user
+        // console.log("Hello, " + fullName + "!");
+
+        //   var word = "Pakistani";
+
+        // // Find index of letter "n"
+        // var index = word.indexOf("n");
+
+        // // Display result
+        // console.log("String: " + word + "<br>");
+        // console.log("Index of 'n': " + index);
+        //  var text = "Hello World";
+
+        // // Find last index of letter "l"
+        // var index = text.lastIndexOf("l");
+
+        // // Display result
+        // console.log("String: " + text + "<br>");
+        // console.log("Last index of 'l': " + index);
+          // String value
+        // var str = "472";
+
+        // // Convert string to number
+        // var num = Number(str);
+
+        // // Display values and types
+        // console.log("Value: " + str + "<br>");
+        // console.log("Type: " + typeof str + "<br><br>");
+
+        // console.log("Value: " + num + "<br>");
+        // console.log("Type: " + typeof num);
